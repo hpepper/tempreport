17e72e825672a07c1660fbebff068ece2ab03550
hpepper <hpepper@where.no>
2018-11-24 12:05:18 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # tempreport
 Read the DHT temp on a Raspberry PI and report it to a graphite host
+
+## Install
+
+login in as pi on Raspberry PI
+
+git clone https://github.com/hpepper/tempreport.git
+
+## Configuration
+You have to create the file etc/graphite_host_addr.dat
+And write the ip or hostname of the graphite server, you want to send the data to
+
+The first value in 'adjust.dat' is the temperature adjustment, the second is humidity.
+
+
+To run every five minutes add to crontab:
+crontab -e
+0,5,10,15,20,25,30,35,40,45,50,55 * * * * /home/pi/tempreport/bin/senddata.py
---
+++ bin/senddata.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# https://dzone.com/articles/python-getting-data-graphite-0
+
+import platform
+import socket
+import time
+import Adafruit_DHT as dht
+
+
+CARBON_PORT = 2003
+
+file = open('/home/pi/tempreport/etc/graphite_host_addr.dat')
+for line in file:
+    fields = line.strip().split()
+    szGraphiteHostAddr = fields[0]
+file.close()
+
+
+szFqdn = platform.node()
+arHostSplit = szFqdn.split(".")
+szHostname = arHostSplit[0]
+
+h,t = dht.read_retry(dht.DHT22, 4)
+file = open('/home/pi/tempreport/etc/adjust.dat')
+for line in file:
+    fields = line.strip().split()
+file.close()
+fTemp = t + float(fields[0])
+fHumidity = h + float(fields[1])
+print ('Adjusted - Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+
+# The number before ':' is the 0-based index of the values of the .format() list
+temperature_message = '{0:s}.temperature {1:0.1f} {2:d}\n'.format(szHostname, fTemp, int(time.time()))
+humidity_message = '{0:s}.bryggers.humidity {1:0.1f} {2:d}\n'.format(szHostname, fHumidity,  int(time.time()))
+
+sock = socket.socket()
+sock.connect((szGrahiteHostAddr, CARBON_PORT))
+sock.sendall(temperature_message)
+sock.sendall(humidity_message)
+sock.close()
