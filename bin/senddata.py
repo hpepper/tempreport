dd0cd3aa24bd3cfc406a3f802d016f94e550084c
hpepper <henrypepr@gmail.com>
2018-12-02 19:35:32 +0100

    Fix hostname error in senddata.py

--- bin/senddata.py
+++ bin/senddata.py
@@ -32,7 +32,7 @@ print ('Adjusted - Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(fTemp, fHumidity)
 
 # The number before ':' is the 0-based index of the values of the .format() list
 temperature_message = '{0:s}.temperature {1:0.1f} {2:d}\n'.format(szHostname, fTemp, int(time.time()))
-humidity_message = '{0:s}.bryggers.humidity {1:0.1f} {2:d}\n'.format(szHostname, fHumidity,  int(time.time()))
+humidity_message = '{0:s}.humidity {1:0.1f} {2:d}\n'.format(szHostname, fHumidity,  int(time.time()))
 
 sock = socket.socket()
 sock.connect((szGraphiteHostAddr, CARBON_PORT))
